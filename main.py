df35208c2becad37fb9d86d93e74a0986b596085
waliyudin sammadikun <waliyudin938@gmail.com>
2023-04-04 14:31:54 +0700

    akun

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import snowflake.connector
-from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import avg, sum, col,lit
-import streamlit as st
 import pandas as pd
 
 st.write('test ss')
