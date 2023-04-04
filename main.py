b92b8a41a5ec4a716ed6fa11072da2d4c10ab6e4
waliyudin sammadikun <waliyudin938@gmail.com>
2023-04-04 13:28:35 +0700

    test connection

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import snowflake.connector
 
 st.write('test ss')
 
+@st.cache_resource():
+def init_connection():
+    return snowflake.connector.connect(**st.secrets['snowflake'], client_session_keep_alive=True)
+
+conn = init_connection()
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query('select * from data 21 where station = 11')
+
+for row in rows:
+    st.write(f'{row[0]}')
