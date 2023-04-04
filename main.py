2618f83c4a0e8cd4f685f99917e334ba35fe5394
waliyudin sammadikun <waliyudin938@gmail.com>
2023-04-04 14:50:57 +0700

    import module

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import snowflake.connector
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta, date
+import datetime
+from numpy import load
+import plotly.express as px
 
 st.write('test ss')
 
@@ -22,4 +28,3 @@ rows = run_query('select * from data21 where station = 11')
 
 data = pd.DataFrame(rows, columns = ['Station', 'pH', 'DO', 'Temp', 'NH4', 'NO3', 'COD', 'BOD', 'LogDate', 'LogTIme'])
 st.write(data)
-#columns = ['Station', 'pH', 'DO', 'Temp', 'NH4', 'NO3', 'COD', 'BOD', 'LogDate', 'LogTIme']
